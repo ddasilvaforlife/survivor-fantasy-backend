97cc3435ca34d16440ebfab980e9804912243897
David Da Silva <ddasilva@Davids-MacBook-Pro.local>
2026-03-27 20:19:38 -0700

    Fix SSL certificate issue for Render deployment

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
